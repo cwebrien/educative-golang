922b5b185c8eb867406b993ca994aa3b3f2ad3e3
cwebrien <cameron@brien.ca>
2021-02-20 15:19:29 -0500

    Adding convertor code

---
+++ cmd/temperature/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "strconv"
+import "encoding/json"
+
+// aliasing type
+type Celsius float32
+type Fahrenheit float32
+
+// Function to convert celsius to fahrenheit
+func toFahrenheit(t Celsius) Fahrenheit {
+	
+	var temp Fahrenheit
+	return temp
+
+}
+
